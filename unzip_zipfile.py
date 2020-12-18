86bea5e7752b271708a12aa7cae928b0705b34de
pyteacher232 <75642329+pyteacher232@users.noreply.github.com>
2020-12-18 14:07:59 +0100

    Update unzip_zipfile.py

--- unzip_zipfile.py
+++ unzip_zipfile.py
@@ -1,4 +1,6 @@
 import zipfile
 
+full_zip_fname = "My_ZIP.zip"
+working_dir = "Output"
 with zipfile.ZipFile(full_zip_fname, 'r') as zip_ref:
-    zip_ref.extractall(self.working_dir)
+    zip_ref.extractall(working_dir)
