e2cce2293f3449a19d9ab58fe83abc50ff20c30a
pyteacher232 <75642329+pyteacher232@users.noreply.github.com>
2020-12-23 22:34:46 +0100

    more advanced logging

--- how to log messages.py
+++ how to log messages.py
@@ -6,3 +6,23 @@ logging.basicConfig(
     format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
+
+####################################################################################################
+
+import logging
+
+class MyFormatter(logging.Formatter):
+    def format(self, record):
+        record.message2 = ""
+        if(record.args):
+            record.func_name = record.args.get("func_name", "Fallback Value")
+        return super().format(record)
+
+logger = logging.getLogger()
+fh = logging.FileHandler(filename='log.txt', mode='a')
+fh.setFormatter(MyFormatter('%(asctime)s --- %(func_name)s --- %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+logging.basicConfig(level=logging.INFO, handlers=[fh])
+
+def write_log(func_name, message):
+    logger.info(message, {"func_name": func_name})
+
