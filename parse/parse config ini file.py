f5371088fbb30c265d173aa82dc7e40cf57d1e6d
pyteacher232 <75642329+pyteacher232@users.noreply.github.com>
2020-12-24 08:53:35 +0100

    2020-12-24 parse_config ini file

--- parse/parse config ini file.py
+++ parse/parse config ini file.py
@@ -3,9 +3,5 @@ import configparser
 # parse config file
 config = configparser.RawConfigParser()
 config.read('config.ini')
-Username = config['DEFAULT']['Username']
-Password = config['DEFAULT']['Password']
-state = config['CODE']['State']
-NPA = config['CODE']['NPA']
-OCN = config['CODE']['OCN']
-parameters = config['CODE']['Quantity of Blocks Requested']
+
+a = config['RECURSIVE=Yes']['type_dir']
