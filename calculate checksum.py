ab618fad00c99d76c47ed7d94f60c6df024105a5
pyteacher232 <75642329+pyteacher232@users.noreply.github.com>
2020-12-28 00:42:04 +0100

    Update calculate checksum.py

--- calculate checksum.py
+++ calculate checksum.py
@@ -1,4 +1,5 @@
 import hashlib
+import glob
 
 def get_checksum(fname):
 
@@ -10,3 +11,21 @@ def get_checksum(fname):
         return md5_hash.hexdigest()
 
 
+def compare_checksums(self, full_csv_fname):
+    print(f"[compare_checksums] full_csv_fname: {full_csv_fname}")
+    backup_csv_files = glob.glob(f"{self.backup_dir}\\EOD_partial_*.csv")
+    backup_csv_files.sort()
+
+    if len(backup_csv_files) > 0:
+        prev_file_checksum = get_checksum(backup_csv_files[-1])
+        curr_file_checksum = get_checksum(full_csv_fname)
+
+        if prev_file_checksum == curr_file_checksum:
+            print(f"\tDone.")
+            return True
+        else:
+            print(f"\tDone.")
+            return False
+    else:
+        print(f"\tDone.")
+        return False
