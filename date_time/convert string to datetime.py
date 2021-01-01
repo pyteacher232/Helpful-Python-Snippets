1884e9770b85318a811472953a5ef182e7a74634
pyteacher232 <75642329+pyteacher232@users.noreply.github.com>
2021-01-02 00:55:24 +0100

    Create convert string to datetime.py

---
+++ date_time/convert string to datetime.py
@@ -0,0 +1,8 @@
+import datetime
+
+date_time_str = '2018-06-29 08:15:27.243860'
+date_time_obj = datetime.datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S.%f')
+
+print('Date:', date_time_obj.date())
+print('Time:', date_time_obj.time())
+print('Date-time:', date_time_obj)
