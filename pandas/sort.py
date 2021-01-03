a805cbef570fb4ecd8975e7269ef278302a56c86
pyteacher232 <75642329+pyteacher232@users.noreply.github.com>
2021-01-03 16:46:49 +0100

    Create sort.py

---
+++ pandas/sort.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame({
+    'col1': ['A', 'A', 'B', np.nan, 'D', 'C'],
+    'col2': [2, 1, 9, 8, 7, 4],
+    'col3': [0, 1, 9, 4, 2, 3],
+    'col4': ['a', 'B', 'c', 'D', 'e', 'F']
+})
+
+df = df.sort_values(by=['col1'])
+
+print(df)
