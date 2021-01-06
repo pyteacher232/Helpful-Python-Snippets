215704d16ae4eae5b25d0bd70402544cc22d6614
pyteacher232 <rlxmgkqm232>
2021-01-06 21:00:42 +0800

    result

--- excel and csv/write excel file with multiple sheets.py
+++ excel and csv/write excel file with multiple sheets.py
@@ -6,3 +6,10 @@ sheet_2022 = wb.create_sheet("2022")
 sheet_2023 = wb.create_sheet("2023")
 sheet_2024 = wb.create_sheet("2024")
 sheet_2025 = wb.create_sheet("2025")
+
+wb.save("Temp.xlsx")
+
+
+def insert_row(sheet, sheet_cur_idx, result_row):
+    for i, elm in enumerate(result_row):
+        sheet.cell(row=sheet_cur_idx, column=i + 1).value = elm
