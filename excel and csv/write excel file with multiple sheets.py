f87b0bab447c7edf273bbe1aece03278bec27ea3
pyteacher232 <rlxmgkqm232>
2021-01-06 20:41:37 +0800

    Update write excel file with multiple sheets.py

---
+++ excel and csv/write excel file with multiple sheets.py
@@ -0,0 +1,8 @@
+import openpyxl
+
+wb = openpyxl.Workbook()
+sheet_2021 = wb.create_sheet("2021")
+sheet_2022 = wb.create_sheet("2022")
+sheet_2023 = wb.create_sheet("2023")
+sheet_2024 = wb.create_sheet("2024")
+sheet_2025 = wb.create_sheet("2025")
