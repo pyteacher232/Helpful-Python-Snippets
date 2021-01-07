55007e5812ff8f89a56c658659d4cd90f2464eaf
pyteacher232 <rlxmgkqm232>
2021-01-08 01:18:21 +0800

    2021-01

--- scraping/read sav file.py
+++ scraping/read sav file.py
@@ -1,33 +1,14 @@
 import multiprocessing as mp
 from time import time
-
 import pandas as pd
 import pyreadstat
+from multiprocessing import freeze_support
 
-def worker(inpt):
-    import pyreadstat
-    offset, chunksize, path = inpt
-    df, meta = pyreadstat.read_sav(path, row_offset=offset, row_limit=chunksize)
-    return df
+if __name__ == '__main__':
+    start_ts = time()
+    # df_spss, meta = pyreadstat.read_sav("Surgery.sav", user_missing=False)
+    df_spss, meta = pyreadstat.read_file_multiprocessing(pyreadstat.read_sav, "Surgery.sav")
 
-# calculate the number of rows in the file
-_, meta = pyreadstat.read_sav("big.sav", metadataonly=True)
-numrows = meta.number_rows
-# calculate number of cores in the machine, this could also be set manually to some number, i.e. 8
-numcores = mp.cpu_count()
-# calculate the chunksize and offsets
-divs = [numrows // numcores + (1 if x < numrows % numcores else 0)  for x in range (numcores) ]
-chunksize = divs[0]
-offsets = [indx*chunksize for indx in range(numcores)]
-# pack the data for the jobs
-jobs = [(x, chunksize, "big.sav") for x in offsets]
+    elapsed_ts = time() - start_ts
+    print(elapsed_ts)
 
-pool = mp.Pool(processes=numcores)
-# let's go!
-t0=time()
-chunks = pool.map(worker, jobs)
-t1=time()
-print(t1-t0) # this prints 29 seconds
-# chunks is a list of dataframes in the right order
-# you can concatenate all the chunks into a single big dataframe if you like
-final = pd.concat(chunks, axis=0, ignore_index=True)
--- scraping/read sav file_multiple_threading.py
+++ scraping/read sav file_multiple_threading.py
@@ -3,28 +3,33 @@ from time import time
 
 import pandas as pd
 import pyreadstat
+import math
 import threading
 
+
 def worker(inpt):
-    import pyreadstat
+    print(inpt)
     offset, chunksize, path = inpt
     df, meta = pyreadstat.read_sav(path, row_offset=offset, row_limit=chunksize)
+    # df, meta = pyreadstat.read_file_in_chunks(pyreadstat.read_sav, path, offset=offset, chunksize=chunksize,
+    #                                           multiprocess=True, num_processes=10)
     return df
+
+
+start_ts = time()
 
 # calculate the number of rows in the file
 _, meta = pyreadstat.read_sav("Surgery.sav", metadataonly=True)
 numrows = meta.number_rows
 # calculate number of cores in the machine, this could also be set manually to some number, i.e. 8
-numcores = mp.cpu_count()
 # calculate the chunksize and offsets
-divs = [numrows // numcores + (1 if x < numrows % numcores else 0)  for x in range (numcores) ]
-chunksize = divs[0]
-offsets = [indx*chunksize for indx in range(numcores)]
+chunksize = 200
+offsets = [indx * chunksize for indx in range(math.ceil(numrows / chunksize))]
 # pack the data for the jobs
 jobs = [(x, chunksize, "Surgery.sav") for x in offsets]
 
 threads = []
-max_threads = 100
+max_threads = 30
 while threads or jobs:
     for thread in threads:
         if not thread.is_alive():
@@ -33,6 +38,8 @@ while threads or jobs:
     while len(threads) < max_threads and jobs:
         job = jobs.pop()
         thread = threading.Thread(target=worker, args=(job,))
-        thread.setDaemon(True)
         thread.start()
         threads.append(thread)
+
+elapsed_ts = time() - start_ts
+print(elapsed_ts)
